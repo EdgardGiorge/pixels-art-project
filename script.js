9b1f51691d4c0c9dddb566858e55a231f1072cf1
EdgardGiorge <edgardgiorge@hotmail.com>
2021-11-22 20:15:55 -0300

    Requisitos 2 e 3

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ const title = document.createElement('h1');
 title.id = 'title';
 title.innerText = titleText;
 body.appendChild(title);
+const main = document.createElement('main');
+body.appendChild(main);
+const div0 = document.createElement('div');
+main.appendChild(div0);
+div0.id = 'color-palette';
+const arraycss = ['color black', 'color blue', 'color green', 'color red'];
+for (let i = 0; i < arraycss.length; i += 1) { // criando as section's , Ref. Mentoria Joel 22/11 as 18:30
+  const section = document.createElement('section');
+  div0.appendChild(section);
+  section[i] = document.getElementsByTagName('section')[i];
+  section.className = arraycss[i];
+}
